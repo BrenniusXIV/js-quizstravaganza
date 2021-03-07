8d9fa06c6ddb18aa0a1341509f474601d2c353dd
Brent B <brentbaughan1@gmail.com>
2021-03-07 12:14:15 -0500

    added some test questions to js, created display areas in html

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let testQuestions = [{question: "What is 10/2?", answers: {a: '3', b: '5', c: '7', d: '2'}, correctAnswer: 'b'}];
+let clientResponses = [];
+
+
+function showQuestions(questions, quizArea){
+    let output = [];
+    let answers;
+
+    questions.forEach(question => {
+
+        
+    });
+}
