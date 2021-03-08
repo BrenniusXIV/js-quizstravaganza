89d8e6dfff840c2e9bb9bb6af8de7329d43a135e
Brent B <brentbaughan1@gmail.com>
2021-03-08 18:55:17 -0500

    working through some ideas, deleted undesired js logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,5 @@
-let testQuestions = [{question: "What is 10/2?", answers: {a: '3', b: '5', c: '7', d: '2'}, correctAnswer: 'b'}];
-let clientResponses = [];
-
-
-function showQuestions(questions, quizArea){
-    let output = [];
-    let answers;
-
-    questions.forEach(question => {
-
-        
-    });
-}
+let testQuestions = [{question: "What is 10/2?", answers: {a: '3', b: '5', c: '7', d: '2'}, correctAnswer: 'b'}, {question: "What is 20/5?", answers: {a: '2', b: '5', c: '6', d: '4'}, correctAnswer: 'd'}]; //array of test question objects
+let clientAnswers = []; //array to be assembled by the quiz taker's responses
+let questionElement = document.createElement("h2"); //this is the actual question text
+let optionsList = document.createElement("p"); //
+let questionsCount; //elapsed questions
