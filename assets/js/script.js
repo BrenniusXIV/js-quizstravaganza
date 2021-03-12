fcf54f8c1b280ec308194bdb8809f5783ef162d2
Brent B <brentbaughan1@gmail.com>
2021-03-11 22:25:25 -0500

    removed submit button because it was pointless; added some comments. working start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
-let testQuestions = [{question: "What is 10/2?", answers: {a: '3', b: '5', c: '7', d: '2'}, correctAnswer: 'b'}, {question: "What is 20/5?", answers: {a: '2', b: '5', c: '6', d: '4'}, correctAnswer: 'd'}]; //array of test question objects
+const testQuestions = [{question: "What is 10/2?", answers: {a: '3', b: '5', c: '7', d: '2'}, correctAnswer: 'b'}, {question: "What is 20/5?", answers: {a: '2', b: '5', c: '6', d: '4'}, correctAnswer: 'd'}]; //array of test question objects
 let clientAnswers = []; //array to be assembled by the quiz taker's responses
 let questionElement = document.createElement("h2"); //this is the actual question text
 let optionsList = document.createElement("p"); //list options in this element
 let questionsCount; //elapsed questions
 let startButton = document.getElementById("startButton"); //define start button
-let submitButton = document.createElement("button");//define submit button
-submitButton.setAttribute("id", "submitButton");//set button id
-let nextButton = document.createElement("button");
-nextButton.setAttribute("id", "nextButton");
+let nextButton = document.createElement("button"); //define next button
+nextButton.setAttribute("id", "nextButton");//give next button unique id
 
+//look at activity 9 from week 5 to see what might be a better structure
 
 
 startButton.addEventListener("click", function() {
@@ -16,8 +15,6 @@ startButton.addEventListener("click", function() {
     document.body.appendChild(questionElement);
     questionElement.textContent = testQuestions[questionsCount].question;
     //document.body.appendChild(optionsList);
-    document.body.appendChild(submitButton);
-    submitButton.textContent = "Submit response";
     document.body.appendChild(nextButton);
     nextButton.textContent = "Next question ->";
     startButton.remove();
