e121d0c6654b48582cccaee7bd69c5c5c25f16ae
Brent B <brentbaughan1@gmail.com>
2021-03-13 18:42:21 -0500

    created question cycler; created response evaluator; made buttons aware of question number

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
-const testQuestions = [{question: "What is 10/2?", answers: {a: '3', b: '5', c: '7', d: '2'}, correctAnswer: 'b'}, {question: "What is 20/5?", answers: {a: '2', b: '5', c: '6', d: '4'}, correctAnswer: 'd'}]; //array of test question objects
+//const testQuestions = [{question: "What is 10/2?", answers: {a: '3', b: '5', c: '7', d: '2'}, correctAnswer: 'b'}, {question: "What is 20/5?", answers: {a: '2', b: '5', c: '6', d: '4'}, correctAnswer: 'd'}]; //array of test question objects
 const quizStart = document.getElementById("quizStart");
 const quizActive = document.getElementById("quizActive");
 const quizResults = document.getElementById("quizResults");
 
 const elements = [quizStart, quizActive, quizResults];
-const showOnly = (elementToShow) => () => {
+
+//the const function definitions below are not hoisted
+
+const showOnly = (elementToShow) => {
     elements.forEach(element => {
         if (element === elementToShow) {
             showElement(element);
@@ -16,6 +19,7 @@ const showOnly = (elementToShow) => () => {
     })
 };
 
+//showElement and hideElement switch css class
 const showElement = element => {
     element.classList.remove('hide');
     element.classList.add('show');
@@ -25,3 +29,11 @@ const hideElement = element => {
     element.classList.remove('show');
     element.classList.add('hide');
 };
+
+function quizStartButton() {
+    showOnly(quizActive);
+    //add timer here
+};
+
+const startButton = document.getElementById("startButton");
+startButton.addEventListener("click", quizStartButton);
