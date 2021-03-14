a168fb63217247a57625b2d79907881b67bde16e
Brent B <brentbaughan1@gmail.com>
2021-03-13 20:49:28 -0500

    added more questions

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,6 +1,30 @@
 let answersFromClient = [];
 let currentQuestion = 0;
-const testQuestions = [{question: "What is 10/2?", answers: {a: '3', b: '5', c: '7', d: '2'}, correctAnswer: '5'}, {question: "What is the color of the sky?", answers: {a: 'hah', b: 'blue', c: 'what', d: '2'}, correctAnswer: 'blue'}, {question: "What is 20/5?", answers: {a: '2', b: '5', c: '6', d: '4'}, correctAnswer: '4'}]; //array of test question objects
+const testQuestions = [{question: 'Inside which HTML element do we put the JavaScript?',
+answers: {a: '<script>', b: '<scripting>', c: '<js>', d: '<javascript>'},
+correctAnswer: '<script>'},
+{question: 'What is the correct syntax for referring to an external script called xxx.js?',
+answers: {a: '<script href="xxx.js">', b: '<script src="xxx.js">', c: '<script name="xxx.js">', d: '<javascript src="xxx.js">'},
+correctAnswer: '<script src="xxx.js">'},
+{question: 'How do you write "Hello World" in an alert box?',
+answers: {a: 'alertBox("Hello World");', b: 'msgBox("Hello World");', c: 'msg("Hellow World");', d: 'alert("Hello World");'},
+correctAnswer: 'alert("Hello World");'},
+{question: 'How do you create a function in JavaScript?',
+answers: {a: 'function:myFunction()', b: 'function myFunction()', c: 'function = myFunction()', d: 'funct myFunction()'},
+correctAnswer: 'function myFunction()'},
+{question: 'How does a FOR loop start?',
+answers: {a: 'for i = 1 to 5', b: 'for (i <= 5; i++)', c: 'for (i =0; i <= 5; i++)', d: 'for (i = 0; i <= 5)'},
+correctAnswer: 'for (i =0; i <= 5; i++)'},
+{question: 'How can you add a comment in JavaScript?',
+answers: {a: '\'This is a comment', b: '//This is a comment', c: '<!--This is a comment-->', d: '##This is a comment'},
+correctAnswer: '//This is a comment'},
+{question: 'What is the correct way to write a JavaScript array?',
+answers: {a: 'var colors = ["red", "green", "blue"]', b: 'var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")', c: 'var colors = "red", "green", "blue"', d: 'var colors = (1:"red", 2:"green", 3:"blue")'},
+correctAnswer: 'var colors = ["red", "green", "blue"]'},
+{question: 'Which event occurs when the user clicks on an HTML element?',
+answers: {a: 'onmouseover', b: 'onchange', c: 'onclick', d: 'onmouseclick'},
+correctAnswer: 'onclick'}];
+//const testQuestions = [{question: "What is 10/2?", answers: {a: '3', b: '5', c: '7', d: '2'}, correctAnswer: '5'}, {question: "What is the color of the sky?", answers: {a: 'hah', b: 'blue', c: 'what', d: '2'}, correctAnswer: 'blue'}, {question: "What is 20/5?", answers: {a: '2', b: '5', c: '6', d: '4'}, correctAnswer: '4'}]; //array of test question objects
 
 let questionText = document.getElementById("questionText");
 
@@ -89,17 +113,6 @@ function handlePrevQuestion() {
 
 };
 
-// function evaluateResponses() {
-//     answersFromClient[currentQuestion] = getCurrentAnswer();
-//     let evaluatedResponse = [];
-//     answersFromClient.forEach(response => {
-//         if (answersFromClient[response] === (testQuestions[response].correctAnswer)) {
-//             evaluatedResponse[response] = true;
-//         } else evaluatedResponse[response] = false;
-//         return evaluatedResponse;
-//     });
-// };
-
 function evaluateResponses() {
     answersFromClient[currentQuestion] = getCurrentAnswer();
     let evaluatedResponses = {boolValues: [], count: 0};
@@ -109,8 +122,15 @@ function evaluateResponses() {
             evaluatedResponses.count++;
         } else evaluatedResponses.boolValues[i] = false;
     };
-
+    console.log(evaluatedResponses);
     localStorage.setItem("correctAnswers", evaluatedResponses.boolValues);
     localStorage.setItem("count", evaluatedResponses.count);
     return evaluatedResponses;
 };
+
+function submitResponses() {
+    evaluateResponses();
+    showOnly(quizResults);
+};
+
+submitButton.addEventListener("click", submitResponses);
