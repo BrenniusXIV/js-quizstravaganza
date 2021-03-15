aa10c43d37c453175cd6ab3c24dc91f46d53de3b
Brent B <brentbaughan1@gmail.com>
2021-03-15 01:35:40 -0400

    created addHighScore function for aggregating recent quiz data

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,2 +1,15 @@
 const recentCorrectAnswers = localStorage.getItem("count");
 const recentAnswersFromClient = localStorage.getItem("correctAnswers");
+const recentTimeStamp = localStorage.getItem("quizTime");
+
+const highScoresArray = [];
+
+function addHighScore() {
+    let scoreObject = {totalCorrect: 0, answersGiven: "", timeCompleted: ""};
+    scoreObject.totalCorrect = recentCorrectAnswers;
+    scoreObject.answersGiven = recentAnswersFromClient;
+    scoreObject.timeCompleted = recentTimeStamp;
+
+    highScoresArray.push(scoreObject);
+
+};
